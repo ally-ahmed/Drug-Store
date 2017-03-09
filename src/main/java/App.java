c8dc646cc17a60e5df1a51fb9c769512ceaa298c
brianmituka <mitukab@outlook.com>
2017-03-09 07:52:03 +0300

    calendar

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -32,6 +32,12 @@ public class App {
 		 String productDescription = request.queryParams("product-description");
 		 int productPrice = Integer.parseInt(request.queryParams("product-price"));
 		 response.redirect("/allProducts");
+		 return null;
+	 }, new VelocityTemplateEngine());
+
+	 get("/calendar", (request, response) -> {
+		 Map<String, Object> model = new HashMap<String, Object>();
+		 model.put("template", "templates/calendar.vtl");
 		 return new ModelAndView(model, layout);
 	 }, new VelocityTemplateEngine());
 
